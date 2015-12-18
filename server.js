627399bfc787b231010aa26a241e8806262056c1
Michael Oryl <michael@oryl.com>
2015-12-18 12:43:16 -0500

    Stub of OAuth2 callback URL on node app

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 var express = require('express')
 var livereload = require('livereload');
 
-var app = express()
+var app = express();
 
-app.use('/node_modules', express.static(__dirname + '/node_modules'))
-app.use('/', express.static(__dirname + '/src'))
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+app.get('/auth/callback', function(req, res) {
+  console.log("Request cookies: %j", req.cookies);
+  console.log("Request originalUrl: %j", req.originalUrl);
+  console.log("Request path: %j", req.path);
+  console.log("Request query: %j", req.query);
+  res.send('Received the callback');
+});
+app.use('/', express.static(__dirname + '/src'));
 
-app.listen(3000)
-console.log('Listening on port 3000.')
+app.listen(3000);
+console.log('Listening on port 3000.');
 
 app = livereload.createServer({
   exts: ['html', 'css', 'js', 'png', 'gif', 'jpg', 'ts']
-})
+});
 
-app.watch(__dirname + '/src')
-console.log('Live reload enabled')
+app.watch(__dirname + '/src');
+console.log('Live reload enabled');
