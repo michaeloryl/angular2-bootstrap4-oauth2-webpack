27181f370e912a998f67ab6d2dfe34c246c0b29d
Michael Oryl <oryl.michael@pennmutual.com>
2015-12-28 14:38:30 -0500

    Using webpack-dev-server; moved angular2 polyfills to new vendor.ts file

---
+++ src/app/vendor.js
@@ -0,0 +1,6 @@
+require('angular2/bundles/angular2-polyfills');
+require('angular2/platform/browser');
+require('angular2/core');
+require('angular2/http');
+require('angular2/router');
+//# sourceMappingURL=vendor.js.map
