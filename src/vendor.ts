ca177707d0cf32c803fe6d55f4a28cffd663a229
Michael Oryl <michael@oryl.com>
2015-12-29 15:51:29 -0500

    Moved Bootstrap and JQuery css and js out of index.html and into vendor.ts

--- src/vendor.ts
+++ src/vendor.ts
@@ -1,3 +1,6 @@
+require('./css/bootstrap.css');
+require('./css/main.css');
+
 import 'angular2/bundles/angular2-polyfills';
 
 import 'angular2/platform/browser';
@@ -5,8 +8,10 @@ import 'angular2/core';
 import 'angular2/http';
 import 'angular2/router';
 
-/*
-import 'js.cookie';
-import 'jquery';
-import 'bootstrap';
+// FOR WHATEVER REASON, THESE MUST COME AFTER THE ANGULAR2-POLYFILLS IMPORT
+require('./lib/jquery/jquery.min.js');
+require('./lib/bootstrap/bootstrap.js');
+
+/* // Can't get this to work through Webpack so far
+require('./lib/js.cookie/js.cookie.js');
 */
